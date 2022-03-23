008f4b277cd2d4b2f953b60a5682e09fa13a3c24
Amlan Kumar <amlankumar@Amlan-Kumar-B605-MacBook-Air.local>
2022-03-23 14:40:50 +0530

    23rd March 2:40 pm

---
+++ cypress/integration/faketest.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Dashboard', function(){
+    
+    it('faketest', function(){
+        cy.visit("https://rahulshettyacademy.com/angularAppdemo/")
+       
+        cy.intercept(
+            {
+                method : 'GET', 
+                url : 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+            },
+            {
+                statusCode : 200,
+                body : [{"book_name":"RestAssured with Java","isbn":"RSU","aisle":"2301"}]
+            }).as('booklist')
+            cy.get('.btn-primary').click()
+            cy.wait('@booklist').should(({request, response})=>{
+                cy.get('tr').should('have.length',response.body.length+1) //if the rows available = response array row length(+1 as the header row is counted as well)
+            })
+            cy.get('p').should('have.text',"Oops only 1 Book available")
+    })
+})
---
+++ cypress/integration/faketest2.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('Dashboard', function(){
+    
+    it('faketest', function(){
+        cy.visit("https://rahulshettyacademy.com/angularAppdemo/")
+       
+        cy.intercept('GET','https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty',
+        (req)=>
+        {
+            req.url='https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=amlan' //here amlan doesn't exist so we asserted status code with 404, else if it existed, then we should assert with 403(forbidden)
+            req.continue((res)=>{
+                expect(res.statusCode).to.equal(404) // if statuscode is returned 200 for other author logins, then its a big bug in the website security system
+            })
+        }).as('booklist')
+            cy.get('.btn-primary').click()
+    })
+})
